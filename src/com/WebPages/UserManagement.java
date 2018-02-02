cf8b898cf0dd3cfc4ef4e1a31e0a7d4f76b8326f
Goni <goni@github.com>
2018-02-02 11:57:06 +0530

    Framework Changes..

---
+++ src/com/WebPages/UserManagement.java
@@ -0,0 +1,16 @@
+package com.WebPages;
+
+import org.openqa.selenium.WebDriver;
+
+import com.BaseSetup.BaseSetUp;
+
+public class UserManagement extends BaseSetUp{
+	
+	
+
+	public UserManagement(WebDriver driver) {
+		super(driver);
+		
+	}
+
+}
--- src/com/e2m/Home/HomePage.java
+++ src/com/e2m/Home/HomePage.java
@@ -1,9 +1,27 @@
 package com.e2m.Home;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
 public class HomePage {
 	
-	public void main(){
+	
+	
+	public static void openHomePage(){
 		
+		System.setProperty("webdriver.gecko.driver","/Users/goni/Documents/workspace/WebSpiders/Event2Mobile/Browser_Driver/geckodriver");
+		
+		WebDriver driver = new FirefoxDriver();
+		
+		driver.get("https://cmsuat2.event2mobile.com/");
+		
+		driver.manage().window().maximize();
+		
+	}
+	
+	public static void main(String args[]){
+		
+		openHomePage();
 	}
 
 }
