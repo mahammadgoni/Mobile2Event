337dd6073433742f780d8ed5da2f604f8bb1a2d9
Goni <goni@github.com>
2018-02-02 20:35:46 +0530

    Add User Changes

--- src/com/UserManagement/AddUser.java
+++ src/com/UserManagement/AddUser.java
@@ -1,16 +1,83 @@
-package com.WebPages;
+package com.UserManagement;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 import com.BaseSetup.BaseSetUp;
 
-public class UserManagement extends BaseSetUp{
+public class AddUser extends BaseSetUp{
+	
+
+	By emailId = By.xpath("//*[@id='txtLogin']");
+	
+	By proceedBtn = By.xpath("//*[@id='btnProceed']");
+	
+	By pass = By.xpath("//*[@id='txtPass']");
+	
+	By signInBtn = By.xpath("//*[@id='btnLogin']");
+	
+	By logoutBtn = By.xpath("//*[@id='Layer_1']");
+	
+	By menu = By.xpath("//a[@class='toggle-menu']");
+
+	By userMgnt = By.xpath("//*[@id='UM']");
+	
+	By user = By.xpath("//*[@id='UMUser']");
+	
+	By addUser = By.xpath("//*[@id='ContentPlaceHolder1_BtnAddNew']");
+	
+	By importUser = By.xpath("//*[@id='ContentPlaceHolder1_btnImport']");
+	
+	By downloadTemplete = By.xpath("//*[@id='ContentPlaceHolder1_btnDataTamplate']");
+	
+	By deleteSelectedUser = By.xpath("//*[@title='Delete']");
+	
+	By deleteUserFromList = By.xpath("//*[@id='ContentPlaceHolder1_gvUser_imgDelFromList_1']");
+	
 	
 	
 
-	public UserManagement(WebDriver driver) {
+	public AddUser(WebDriver driver) {
 		super(driver);
 		
+	}
+	
+	
+	public AddUser addUser(String EmailId,String Password){
+		
+		System.out.println("Entering Email id  is : "+EmailId);
+		
+		waitForClickabilityOf(emailId);
+		
+		driver.findElement(emailId).sendKeys(EmailId);
+		
+		System.out.println("Clicking on Proceed Button");
+		
+		waitForClickabilityOf(proceedBtn);
+		
+		driver.findElement(proceedBtn).click();
+				
+		System.out.println("Entering Password  is : "+Password);
+		
+		waitForClickabilityOf(pass);
+		
+		driver.findElement(pass).sendKeys(Password);
+		
+		System.out.println("Clicking on Sign in Button");
+		
+		waitForClickabilityOf(signInBtn);
+		
+		driver.findElement(signInBtn).click();
+		
+		
+		return new AddUser(driver);
+		
+	}
+	
+	public AddUser userBulkUpload(){
+		
+		
+		return new AddUser(driver);
 	}
 
 }
--- src/com/Utils/BrowserSetUp.java
+++ src/com/Utils/BrowserSetUp.java
@@ -1,6 +1,10 @@
 package com.Utils;
 
+import java.util.concurrent.TimeUnit;
+
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class BrowserSetUp {
 	
@@ -8,8 +12,95 @@ public class BrowserSetUp {
 	
 	String geckoPath = "/Users/goni/Documents/workspace/WebSpiders/Event2Mobile/Browser_Driver/geckodriver";
 	
-	String chromeDrPath = "";
+	String chromeDrPath = "/Users/goni/Documents/workspace/WebSpiders/Event2Mobile/Browser_Driver/chromedriver";
+	
+	String logPath = "/Users/goni/Documents/workspace/WebSpiders/Event2Mobile/Browser_Driver/firefoxLogs";
 	
 	String e2mURL = "https://cmsuat2.event2mobile.com/";
+	
+	
+	
+	protected void openBrowser(String BrowserName){
+		
+		if (BrowserName=="Chrome") {
+			
+			System.out.println("===============================================");
+			
+			System.out.println("Opening the Chrome Browser");
+			
+			System.setProperty("webdriver.chrome.driver", chromeDrPath);
+			
+			driver = new ChromeDriver();
+			
+		    driver.manage().window().fullscreen();
+			
+//			driver.manage().window().maximize();
+			
+			driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+			
+			System.out.println("Opening the Url : "+e2mURL);
+			
+			driver.get(e2mURL);
+			
+		} else {
+			
+			System.out.println("===============================================");
+			
+			System.out.println("Opening the Firefox Browser");
+			
+			System.setProperty("webdriver.gecko.driver", geckoPath);
+			
+//			This code will store all unwanted Firefox warnings to a file
+			
+			System.setProperty(FirefoxDriver.SystemProperty.DRIVER_USE_MARIONETTE, "true");
+			
+			System.setProperty(FirefoxDriver.SystemProperty.BROWSER_LOGFILE, logPath);
+			
+			driver  = new FirefoxDriver();
+			
+//			String currentWindow  = this.driver.getWindowHandle();
+//			
+//			driver.switchTo().window(currentWindow);
+//			
+//			driver.switchTo().defaultContent();
+			
+//			((JavascriptExecutor)driver).executeScript("window.focus();");
+			
+//			Dimension dm = new Dimension(1382, 744);
+//			
+//			driver.manage().window().setSize(dm);
+			
+			driver.manage().window().fullscreen();
+			
+//			driver.manage().window().setPosition(new Point(-2000, 0));
+			
+//			driver.manage().window().maximize();
+			
+			driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+			
+			System.out.println("Opening the Url : "+e2mURL);
+			
+			driver.get(e2mURL);
+			
+
+		}
+		
+		
+	}
+	
+	protected void closeBrowser(){
+		
+		System.out.println("Closing the current Browser Tab");
+		
+		driver.close();
+	}
+	
+	protected void quitAllBrowser(){
+		
+		System.out.println("Closing Browser");
+		
+		driver.quit();
+		
+	}
 
 }
